04261f9aa574bc7a0e07b3c4fdd7916fd1423a94
Ichinaro <65051884+Ichinaro@users.noreply.github.com>
2024-07-04 07:03:07 +0000

    Configurar EF

--- obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651794abf4a7ddf82d7d40aa8801e16415b066f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0211d9b208f754d7fee336ac6ef1ea8b8955ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
