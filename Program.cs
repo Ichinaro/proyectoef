bca2ead6a31589eaa0026a3230211a16a38bc807
Ichinaro <65051884+Ichinaro@users.noreply.github.com>
2024-07-04 12:43:42 +0000

    DB en Memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Mvc; // Importa el espacio de nombres necesario para utilizar características de ASP.NET Core MVC. como [FromServices]
+using Microsoft.EntityFrameworkCore; // Importa el espacio de nombres necesario para trabajar con Entity Framework Core.
+using proyectoef; // Importa el espacio de nombres de tu proyecto, que contiene los modelos y el contexto de datos
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args); // Crea un constructor de aplicación web que configura los servicios y la aplicación web ASP.NET Core.
 
-app.Run();
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB")); 
+// Registra el contexto de datos Tareascontext en el contenedor de Servicios de la aplicación, 
+// configurándolo para usar una base de datos en memoria llamada "TareasDB".
+
+var app = builder.Build(); // Construye la aplicación web a partir del constructor configurado.
+
+app.MapGet("/", () => "Hell0 world!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbcontext) => {
+  // Mapea una ruta HTTP GET "/dbconexion" a una función asincrónica que recibe un Tareascontext inyectado desde los servicios.
+
+  dbcontext.Database.EnsureCreated(); 
+  // Asegura que la base de datos en memoria esté creada. 
+  // Este método crea la base de datos y el esquema si no existen.
+
+  return Results.Ok("Base de datos en memoria: " + dbcontext.Database.IsInMemory()); 
+  // Devuelve un resultado HTTP 200 OK con un mensaje indicando si la base de datos está en memoria.
+});
+
+app.Run(); // Inicia la aplicación web ASP.NET Core y comienza a escuchar las solicitudes HTTP.
--- obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyectoef.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04261f9aa574bc7a0e07b3c4fdd7916fd1423a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa929238b738998cbcba027ed2827ac096776d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
